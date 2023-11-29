450affdf63807b9e36b0b2fe23e673a76b7fdf7a
Daniella McNair <daniella.mcnair495@gmail.com>
2023-11-28 18:00:47 -0600

    some progress made

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,5 +1,9 @@
-import { deleteBook, getBooks } from '../components/api/bookData';
+import {
+  deleteBook, getBooks, createBook, getSingleBook, updateBook
+} from '../components/api/bookData';
 import { showBooks } from '../pages/books';
+import addBookForm from '../components/forms/addBookForm';
+import payload from './formEvents';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -15,16 +19,40 @@ const domEvents = () => {
       }
     }
 
+    // TODO: CLICK EVENT FOR SHOWING FORM FOR SUBMITTING A BOOK
+    if (e.target.id.includes('submit-book')) {
+      const payload = {
+        title: document.querySelector('#title').value,
+        description: document.querySelector('#description').value,
+        image: document.querySelector('#image').value,
+        price: document.querySelector('#price').value,
+        author_id: document.querySelector('#author_id').value,
+        sale: document.querySelector('#sale').checked,
+      };
+
+      createBook(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+
+        updateBook(patchPayload).then(() => {
+          getBooks().then(showBooks);
+        });
+      });
+    }
+
     // TODO: CLICK EVENT FOR SHOWING FORM FOR ADDING A BOOK
     if (e.target.id.includes('add-book-btn')) {
-      console.warn('ADD BOOK');
+      addBookForm();
     }
 
     // TODO: CLICK EVENT EDITING/UPDATING A BOOK
     if (e.target.id.includes('edit-book-btn')) {
-      console.warn('EDIT BOOK', e.target.id);
-      console.warn(e.target.id.split('--'));
+      const [, firebaseKey] = e.target.id.split('--');
+
+      getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
     }
+
+    getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
+    // getSingleBook(firebaseKey).then(addBookForm); // using the callback method
     // TODO: CLICK EVENT FOR VIEW BOOK DETAILS
     if (e.target.id.includes('view-book-btn')) {
       console.warn('VIEW BOOK', e.target.id);
--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,6 +1,7 @@
 import { signOut } from '../utils/auth';
 import { getBooks, booksOnSale } from '../components/api/bookData';
 import { showBooks } from '../pages/books';
+import { getAuthorBooks, getAuthors } from '../components/api/authorData';
 
 // navigation events
 const navigationEvents = () => {
@@ -20,6 +21,9 @@ const navigationEvents = () => {
 
   // FIXME: STUDENTS Create an event listener for the Authors
   // 1. When a user clicks the authors link, make a call to firebase to get all authors
+  document.querySelector('#all-books').addEventListener('click', () => {
+    getAuthors().then(getAuthorBooks);
+  });
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
